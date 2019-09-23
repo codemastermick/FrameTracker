884891388f1d1d76688087b4b8f2aba7dadbc4c6
Mick Evans <codemastermick@gmail.com>
2019-09-23 00:30:50 -0400

    feat: add support for getting all weapons by name

--- src/app/shared/wf-data.service.ts
+++ src/app/shared/wf-data.service.ts
@@ -8,10 +8,21 @@ import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 export class WfDataService {
   allFrames: Item[];
   allPrimaries: Item[];
+  allSecondaries: Item[];
+  allMelee: Item[];
 
   constructor(private sanitization: DomSanitizer) {
     this.allFrames = this.getAllWarframes();
     this.allPrimaries = this.getAllPrimaries();
+    this.allSecondaries = this.getAllSecondaries();
+    this.allMelee = this.getAllMelees();
+  }
+
+  private formatUrl(url: string): string {
+    url = url.toLowerCase();
+    url = url.replace('%20', ' ');
+    url = url.replace(/\b(\w)/g, c => c.toUpperCase());
+    return url;
   }
 
   public getThumbnail(imageName: string): SafeStyle {
@@ -23,9 +34,7 @@ export class WfDataService {
   }
 
   getWarframeByName(name: string): Item {
-    name = name.toLowerCase();
-    name = name.replace('%20', ' ');
-    name = name.replace(/\b(\w)/g, c => c.toUpperCase());
+    name = this.formatUrl(name);
     console.log(`Retreiving data for ${name}`);
     return this.allFrames.find(x => x.name === name);
   }
@@ -35,9 +44,7 @@ export class WfDataService {
   }
 
   getPrimaryByName(name: string): Item {
-    name = name.toLowerCase();
-    name = name.replace('%20', ' ');
-    name = name.replace(/\b(\w)/g, c => c.toUpperCase());
+    name = this.formatUrl(name);
     console.log(`Retreiving data for ${name}`);
     return this.allPrimaries.find(x => x.name === name);
   }
@@ -46,7 +53,19 @@ export class WfDataService {
     return new Items({ category: ['Secondary'] });
   }
 
+  getSecondaryByName(name: string): Item {
+    name = this.formatUrl(name);
+    console.log(`Retreiving data for ${name}`);
+    return this.allSecondaries.find(x => x.name === name);
+  }
+
   getAllMelees(): Item[] {
     return new Items({ category: ['Melee'] });
+  }
+
+  getMeleeByName(name: string): Item {
+    name = this.formatUrl(name);
+    console.log(`Retreiving data for ${name}`);
+    return this.allMelee.find(x => x.name === name);
   }
 }
