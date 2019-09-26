32ea7d32a7fbb08b697b5523e8e3cd2345524392
Mick Evans <codemastermick@gmail.com>
2019-09-25 21:38:40 -0400

    test(app): write basic non sticky test for navbar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/material.module';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -26,5 +27,11 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('The Sanctuary');
+  });
+
+  it('should have the navbar not sticky at first', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const container = fixture.debugElement.query(By.css('.sticky-nav'));
+    expect(container).toBeFalsy();
   });
 });
