cb97b66a4dfa2c0bd91bd7bd47251f4dfb1b0228
Mick Evans <codemastermick@gmail.com>
2019-09-30 10:18:26 -0400

    feat: create defer load directive

---
+++ src/app/shared/defer-load-directive.ts
@@ -0,0 +1,44 @@
+import { ElementRef, EventEmitter, Output, AfterViewInit, Directive, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Directive({
+  selector: '[deferLoad]'
+})
+export class DeferLoadDirective implements AfterViewInit {
+  @Output() public deferLoad: EventEmitter<any> = new EventEmitter();
+
+  private intersectionObserver?: IntersectionObserver;
+
+  constructor(
+    private element: ElementRef
+  ) { }
+
+  public ngAfterViewInit() {
+    this.intersectionObserver = new IntersectionObserver(entries => {
+      this.checkForIntersection(entries);
+    }, {});
+    this.intersectionObserver.observe((this.element.nativeElement) as Element);
+  }
+
+  private checkForIntersection = (entries: Array<IntersectionObserverEntry>) => {
+    entries.forEach((entry: IntersectionObserverEntry) => {
+      if (this.checkIfIntersecting(entry)) {
+        this.deferLoad.emit();
+        this.intersectionObserver.unobserve((this.element.nativeElement) as Element);
+        this.intersectionObserver.disconnect();
+      }
+    });
+  }
+
+  private checkIfIntersecting(entry: IntersectionObserverEntry) {
+    return (entry as any).isIntersecting && entry.target === this.element.nativeElement;
+  }
+}
+
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [DeferLoadDirective],
+  exports: [DeferLoadDirective]
+})
+export class DeferLoadModule { }
