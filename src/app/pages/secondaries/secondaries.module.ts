9fea471e0825e5d71c4ed2cc4016d4ed6a51503f
Mick Evans <codemastermick@gmail.com>
2019-10-04 14:38:29 -0400

    fix: replace component declaration with module import

--- src/app/pages/secondaries/secondaries.module.ts
+++ src/app/pages/secondaries/secondaries.module.ts
@@ -4,8 +4,8 @@ import { SecondariesRoutingModule } from './secondaries-routing.module';
 import { SecondariesComponent } from './secondaries.component';
 import { MaterialModule } from '../../shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { SecondarySummaryComponent } from 'app/components/secondary-summary/secondary-summary.component';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { SecondarySummaryModule } from 'app/components/secondary-summary/secondary-summary.module';
 
 @NgModule({
   imports: [
@@ -13,8 +13,9 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
     SecondariesRoutingModule,
     MaterialModule,
     FlexLayoutModule,
-    ScrollingModule
+    ScrollingModule,
+    SecondarySummaryModule
   ],
-  declarations: [SecondariesComponent, SecondarySummaryComponent]
+  declarations: [SecondariesComponent]
 })
 export class SecondariesModule { }
