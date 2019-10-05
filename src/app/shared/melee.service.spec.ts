c5336e342c9be52ff425734d5a3a2dd2c14ebf45
Mick Evans <codemastermick@gmail.com>
2019-10-05 13:51:27 -0400

    test(melee service): write tests for thumbnail corrections

--- src/app/shared/melee.service.spec.ts
+++ src/app/shared/melee.service.spec.ts
@@ -9,4 +9,34 @@ describe('MeleeService', () => {
     const service: MeleeService = TestBed.get(MeleeService);
     expect(service).toBeTruthy();
   });
+
+  it('should correct Ceramic Dagger', () => {
+    const service: MeleeService = TestBed.get(MeleeService);
+    const val = service.getThumb('ceramic-dagger.png');
+    expect(val.toString()).toContain('SwordCeramic_d.png');
+  });
+
+  it('should correct Dark Split Sword', () => {
+    const service: MeleeService = TestBed.get(MeleeService);
+    const val = service.getThumb('dark-split-sword.png');
+    expect(val.toString()).toContain('DarkSplitSwordDualIcon.png');
+  });
+
+  it('should correct Dual Ether', () => {
+    const service: MeleeService = TestBed.get(MeleeService);
+    const val = service.getThumb('dual-ether.png');
+    expect(val.toString()).toContain('Dual_Ether.png');
+  });
+
+  it('should correct Galatine', () => {
+    const service: MeleeService = TestBed.get(MeleeService);
+    const val = service.getThumb('galatine.png');
+    expect(val.toString()).toContain('Galatine_o.png');
+  });
+
+  it('should correct Wolf Sledge', () => {
+    const service: MeleeService = TestBed.get(MeleeService);
+    const val = service.getThumb('wolf-sledge.png');
+    expect(val.toString()).toContain('Wolf_Sledge_Image.png');
+  });
 });
