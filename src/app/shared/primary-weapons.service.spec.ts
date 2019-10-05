7e15a4ef06373d2719fafabb3af04263ecaf6742
Mick Evans <codemastermick@gmail.com>
2019-10-05 13:53:43 -0400

    test(primary service): write tests for thumbnail corrections

--- src/app/shared/primary-weapons.service.spec.ts
+++ src/app/shared/primary-weapons.service.spec.ts
@@ -9,4 +9,16 @@ describe('PrimaryWeaponsService', () => {
     const service: PrimaryWeaponsService = TestBed.get(PrimaryWeaponsService);
     expect(service).toBeTruthy();
   });
+
+  it('should correct Miter', () => {
+    const service: PrimaryWeaponsService = TestBed.get(PrimaryWeaponsService);
+    const val = service.getThumb('miter.png');
+    expect(val.toString()).toContain('miter.png');
+  });
+
+  it('should correct Sybaris Prime', () => {
+    const service: PrimaryWeaponsService = TestBed.get(PrimaryWeaponsService);
+    const val = service.getThumb('sybaris-prime.png');
+    expect(val.toString()).toContain('sybaris-prime.png');
+  });
 });
