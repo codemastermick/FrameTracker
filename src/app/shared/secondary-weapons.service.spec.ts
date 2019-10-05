a110b4c94bccc66a16e6199cb38abd620a3ee916
Mick Evans <codemastermick@gmail.com>
2019-10-05 13:55:51 -0400

    test(secondary service): write test to check if type filtering works

--- src/app/shared/secondary-weapons.service.spec.ts
+++ src/app/shared/secondary-weapons.service.spec.ts
@@ -9,4 +9,9 @@ describe('SecondaryWeaponsService', () => {
     const service: SecondaryWeaponsService = TestBed.get(SecondaryWeaponsService);
     expect(service).toBeTruthy();
   });
+
+  it('should be return a filtered list when asked', () => {
+    const service: SecondaryWeaponsService = TestBed.get(SecondaryWeaponsService);
+    expect(service.getAllOfType('Pistol')).toBeTruthy();
+  });
 });
