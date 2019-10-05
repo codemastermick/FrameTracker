51c23d3f6d919a8b8d19662330d564185467c1be
Mick Evans <codemastermick@gmail.com>
2019-10-05 16:28:07 -0400

    chore: doublequote variables in travis scripts

--- .travis/skip_on_readme.sh
+++ .travis/skip_on_readme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(.md$)'
+if ! git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep -qvE '(.md$)'
 then
   echo "Only docs were updated, not running the CI."
   exit
