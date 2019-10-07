56f0fd69049e6bfce942d34eaae8cd6102a25168
Mick Evans <codemastermick@gmail.com>
2019-10-07 09:10:13 -0400

    test(wf-data service): write coverage tests

--- src/app/shared/wf-data.service.spec.ts
+++ src/app/shared/wf-data.service.spec.ts
@@ -9,4 +9,28 @@ describe("WfDataService", () => {
     const service: WfDataService = TestBed.get(WfDataService);
     expect(service).toBeTruthy();
   });
+
+  it("should correct Equinox", () => {
+    const service: WfDataService = TestBed.get(WfDataService);
+    const val = service.getThumb("equinox.png");
+    expect(val.toString()).toContain("EquinoxIcon64.png");
+  });
+
+  it("should correct Equinox Prime", () => {
+    const service: WfDataService = TestBed.get(WfDataService);
+    const val = service.getThumb("equinox-prime.png");
+    expect(val.toString()).toContain("EquinoxPrimeIcon64.png");
+  });
+
+  it("should correct Wukong Prime", () => {
+    const service: WfDataService = TestBed.get(WfDataService);
+    const val = service.getThumb("wukong-prime.png");
+    expect(val.toString()).toContain("WukongPrimeIcon64.png");
+  });
+
+  it("should correct Chroma Prime", () => {
+    const service: WfDataService = TestBed.get(WfDataService);
+    const val = service.getThumb("https://vignette.wikia.nocookie.net/warframe/images/f/f2/ChromaPrime.png/revision/latest?cb=20180925193634");
+    expect(val.toString()).toContain("ChromaPrimeIcon64.png");
+  });
 });
