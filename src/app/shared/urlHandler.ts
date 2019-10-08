fc6f8f6ae5f19c8c611c37a24de552cfb6a96732
Mick Evans <codemastermick@gmail.com>
2019-10-08 14:01:13 -0400

    chore: codacy told me to brack this

--- src/app/shared/urlHandler.ts
+++ src/app/shared/urlHandler.ts
@@ -1,6 +1,6 @@
 export function formatUrl(url: string): string {
   url = url.toLowerCase();
   url = url.replace(/\%20.*?|[ ]/gm, " ");
-  url = url.replace(/\b(\w)/g, c => c.toUpperCase());
+  url = url.replace(/\b(\w)/g, (c) => c.toUpperCase());
   return url;
 }
