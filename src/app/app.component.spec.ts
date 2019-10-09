4726235d496d3465550da3bb1a62210eb12a8f3e
Mick Evans <codemastermick@gmail.com>
2019-10-09 04:34:59 -0400

    test: write coverage tests for nav events

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
 import { MaterialModule } from "./shared/material.module";
+import { NavigationEnd, Router, NavigationStart, NavigationCancel, NavigationError } from "@angular/router";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
@@ -26,5 +27,45 @@ describe("AppComponent", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual("The Sanctuary");
+  });
+
+  it("should display the spinner while loading", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    const event = new NavigationStart(42, "/");
+    TestBed.get(Router).events.next(event);
+
+    expect(app.loading).toBeTruthy();
+  });
+
+  it("should hide the spinner after loading", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    const event = new NavigationEnd(42, "/", "/");
+    TestBed.get(Router).events.next(event);
+
+    expect(app.loading).toBeFalsy();
+  });
+
+  it("should hide the spinner on load cancel", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    const event = new NavigationCancel(42, "/", "/");
+    TestBed.get(Router).events.next(event);
+
+    expect(app.loading).toBeFalsy();
+  });
+
+  it("should hide the spinner on navigation error", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+
+    const event = new NavigationError(42, "/", "/");
+    TestBed.get(Router).events.next(event);
+
+    expect(app.loading).toBeFalsy();
   });
 });
